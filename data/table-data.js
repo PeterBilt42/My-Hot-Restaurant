1c88edd0945299eb43957b4d6de6ceabed3c15f5
Tyler Peterson <trpeterson42@gmail.com>
2017-07-22 00:22:58 -0700

    fixed files

---
+++ data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+	{
+		costomerName: 'BossGuy',
+		costomerEmail: 'bossguy@example.com',
+		costomerID: 'abc123',
+		phoneNumber: '000-000-0000'
+	}
+];
+
+module.exports = tableArray;
---
+++ data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+	{
+		costomerName: 'OtherGuy',
+		costomerEmail: 'Otherguy@example.com',
+		costomerID: 'xyz789',
+		phoneNumber: '000-000-0000'
+	}
+];
+
+module.exports = waitingArray;
